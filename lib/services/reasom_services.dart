c2176f6e6cc37881a65b8d571dbe9ab7634d5e89
Shubham Gupta <shubhamcmp695@gmail.com>
2025-09-19 10:15:40 +0530

    Add files via upload

--- lib/services/reasom_services.dart
+++ lib/services/reasom_services.dart
@@ -1,15 +1,16 @@
 // reason_service.dart
 
 import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 import '../model/reason_item_model.dart';
 
-
 class ReasonService {
   static Future<List<ReasonItem>> fetchReasons(String leadId) async {
-    final url =
-    Uri.parse("https://fms.bizipac.com/ws/reason.php?leadid=$leadId");
+    final url = Uri.parse(
+      "https://fms.bizipac.com/apinew/ws_new/reason.php?leadid=$leadId",
+    );
 
     final response = await http.get(url);
 
--- lib/services/timeslot_service.dart
+++ lib/services/timeslot_service.dart
@@ -1,16 +1,18 @@
 import 'dart:convert';
-import 'package:http/http.dart' as http;
-import '../model/time_slot_model.dart';
 
+import 'package:http/http.dart' as http;
+
+import '../model/time_slot_model.dart';
 
 class TimeslotService {
   static Future<List<Timeslot>> fetchTimeSlots() async {
-    const url = "https://fms.bizipac.com/ws/time_slot.php"; // Your actual API URL
+    const url =
+        "https://fms.bizipac.com/apinew/ws_new/time_slot.php"; // Your actual API URL
     final response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
       final jsonData = json.decode(response.body);
-      
+
       if (jsonData['success'] == 1) {
         List<Timeslot> slots = (jsonData['data'] as List)
             .map((item) => Timeslot.fromJson(item))
