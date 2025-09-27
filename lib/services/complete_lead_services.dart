cb3c4f9ddb7dec09d3958e3a6613273c47979972
Shubham Gupta <shubhamcmp695@gmail.com>
2025-09-27 11:05:43 +0530

    Add files via upload

--- lib/services/complete_lead_services.dart
+++ lib/services/complete_lead_services.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:peckme/model/complete_lead_model.dart';
 
-
 class CompleteLeadServices {
-  final String baseUrl = "https://fms.bizipac.com/apinew/ws_new/complete_lead.php?"; // change this
+  final String baseUrl =
+      "https://fms.bizipac.com/apinew/ws_new/complete_lead.php?"; // change this
 
   Future<CompleteLeadModel> completeLead({
     required String loginId,
@@ -13,11 +14,11 @@ class CompleteLeadServices {
     final url = Uri.parse("$baseUrl");
     final response = await http.post(
       url,
-      body: {
-        "loginid": loginId,
-        "leadid": leadId,
-      },
+      body: {"loginid": loginId, "leadid": leadId},
     );
+    print("----------------");
+    print(response.body);
+    print("----------------");
 
     if (response.statusCode == 200) {
       final jsonBody = json.decode(response.body);
